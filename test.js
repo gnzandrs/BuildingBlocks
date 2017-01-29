4bd70b3f3cc9fe53b81f374055fc3486baba7d18
gnzandrs <gavr@outlook.cl>
2017-01-29 20:52:42 -0300

    listing cities on index

--- test.js
+++ test.js
@@ -6,12 +6,41 @@ describe('Request to the root path', function () {
   it('Return a 200 status code', function (done) {
     request(app)
       .get('/')
-      .expect(200)
-      .end(function (error) {
-        if (error) throw error
-        done()
-      })
+      .expect(200, done)
+  })
 
+  it('Returns a html format', function (done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/, done)
+  })
+
+  it('Returns an inde file with cities', function (done) {
+    request(app)
+      .get('/')
+      .expect(/cities/i, done)
+  })
+
+})
+
+describe('Listing cities on /cities', function () {
+
+  it('Returns 200 status code', function (done) {
+    request(app)
+      .get('/cities')
+      .expect(200, done)
+  })
+
+  it ('Returns JSON format', function (done) {
+    request(app)
+      .get('/cities')
+      .expect('Content-Type', /json/, done)
+  })
+
+  it ('Returns initial cities', function (done) {
+    request(app)
+      .get('/cities')
+      .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done)
   })
 
 })
